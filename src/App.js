d8e7698647bc25e7817ab90778c8206b15662a23
mizu32 <mizu@tutanota.de>
2021-10-03 04:26:43 +0900

    4

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  header(
-    <header>Dogアプリ</header>
-  )
   return (
-    <div>
-      <h2></h2>
-    </div>
-  )
+    <header>Dogアプリ</header>
+    )
 }
