7a99cb97334a5552ab5892f6736ba00b15133d73
mizu32 <mizu@tutanota.de>
2021-10-03 15:08:02 +0900

    add 1st

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React, { useState } from 'react';
+
+//import * as React from 'react'
+//import React, { useState } from 'react';
 import './App.css'
 
 /**
@@ -8,11 +11,14 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/dachshund/dachshund_4.jpg");
+
   return (
     <div>
-    <header><h1>Dogアプリ</h1></header>
-    犬のサイトです．
-    <img src="https://images.dog.ceo/breeds/dachshund/dachshund_4.jpg"></img>
+      <header><h1>Dogアプリ</h1></header>
+      犬のサイトです．
+      <img src={dogUrl}></img>
     </div>
     )
 }
